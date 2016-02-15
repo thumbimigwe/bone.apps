cdeb40b1c64851cfebc1f61b5f9c9f92b82543c0
thumbimigwe <thumbimigwe@gmail.com>
2016-02-16 02:02:40 +0300

    save

--- blogii/blog/lib/router.js
+++ blogii/blog/lib/router.js
@@ -1,7 +1,12 @@
-// router.configure({
-//     layoutTemplate: 'mainLaout'
-// });
-// router.route('/', {
-//     name: 'root',
-//     controller: 'mainPageController'
-// })
+router.configure({
+    //     layoutTemplate: 'mainLaout'
+});
+router.route('/', {
+    name: 'root',
+    controller: 'mainPageController'
+});
+
+router.route('/new', {
+    name: 'newPost',
+    controller: 'baseController'
+});
